390f022ce00188fe6aca7e193f3bfc40f6693855
henz <hendrimardani290301@gmail.com>
2024-11-30 00:09:42 +0700

    cek-pasien

--- resources/views/cek-pasien.blade.php
+++ resources/views/cek-pasien.blade.php
@@ -1,128 +1,104 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="w-[5440px] h-[1960px] relative bg-white">
-    <svg width="701" height="1013" viewBox="0 0 701 1013" fill="none" xmlns="http://www.w3.org/2000/svg"
-        class="absolute left-[-196px] top-[131px]" preserveAspectRatio="none">
-        <g filter="url(#filter0_f_111_1558)">
-            <ellipse cx="72" cy="391.5" rx="267" ry="259.5" fill="#183E9F"></ellipse>
-        </g>
-        <defs>
-            <filter id="filter0_f_111_1558" x="-556.8" y="-229.8" width="1257.6" height="1242.6"
-                filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
-                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
-                <feGaussianBlur stdDeviation="180.9" result="effect1_foregroundBlur_111_1558"></feGaussianBlur>
-            </filter>
-        </defs>
-    </svg><svg width="697" height="1483" viewBox="0 0 697 1483" fill="none" xmlns="http://www.w3.org/2000/svg"
-        class="absolute left-[1104px] top-[519px]" preserveAspectRatio="none">
-        <g filter="url(#filter0_f_111_1559)">
-            <ellipse cx="745.5" cy="741.5" rx="383.5" ry="379.5" fill="#183E9F"></ellipse>
-        </g>
-        <defs>
-            <filter id="filter0_f_111_1559" x="0.200012" y="0.200012" width="1490.6" height="1482.6"
-                filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
-                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
-                <feGaussianBlur stdDeviation="180.9" result="effect1_foregroundBlur_111_1559"></feGaussianBlur>
-            </filter>
-        </defs>
-    </svg>
-    <mask id="mask0_106_1466" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="30" height="30">
-        <rect width="30" height="30" fill="#D9D9D9"></rect>
-    </mask>
-    <g mask="url(#mask0_106_1466)">
-        <path
-            d="M15 15C13.625 15 12.4479 14.5104 11.4688 13.5312C10.4896 12.5521 10 11.375 10 10C10 8.625 10.4896 7.44792 11.4688 6.46875C12.4479 5.48958 13.625 5 15 5C16.375 5 17.5521 5.48958 18.5312 6.46875C19.5104 7.44792 20 8.625 20 10C20 11.375 19.5104 12.5521 18.5312 13.5312C17.5521 14.5104 16.375 15 15 15ZM5 25V21.5C5 20.7917 5.18229 20.1406 5.54688 19.5469C5.91146 18.9531 6.39583 18.5 7 18.1875C8.29167 17.5417 9.60417 17.0573 10.9375 16.7344C12.2708 16.4115 13.625 16.25 15 16.25C16.375 16.25 17.7292 16.4115 19.0625 16.7344C20.3958 17.0573 21.7083 17.5417 23 18.1875C23.6042 18.5 24.0885 18.9531 24.4531 19.5469C24.8177 20.1406 25 20.7917 25 21.5V25H5ZM7.5 22.5H22.5V21.5C22.5 21.2708 22.4427 21.0625 22.3281 20.875C22.2135 20.6875 22.0625 20.5417 21.875 20.4375C20.75 19.875 19.6146 19.4531 18.4688 19.1719C17.3229 18.8906 16.1667 18.75 15 18.75C13.8333 18.75 12.6771 18.8906 11.5312 19.1719C10.3854 19.4531 9.25 19.875 8.125 20.4375C7.9375 20.5417 7.78646 20.6875 7.67188 20.875C7.55729 21.0625 7.5 21.2708 7.5 21.5V22.5ZM15 12.5C15.6875 12.5 16.276 12.2552 16.7656 11.7656C17.2552 11.276 17.5 10.6875 17.5 10C17.5 9.3125 17.2552 8.72396 16.7656 8.23438C16.276 7.74479 15.6875 7.5 15 7.5C14.3125 7.5 13.724 7.74479 13.2344 8.23438C12.7448 8.72396 12.5 9.3125 12.5 10C12.5 10.6875 12.7448 11.276 13.2344 11.7656C13.724 12.2552 14.3125 12.5 15 12.5Z"
-            fill="#1C1B1F"></path>
-    </g>
-    </svg>
-    <p class="w-[434px] h-[153px] absolute left-[503px] top-[137px] text-[64px] font-bold text-center text-black">
-        <span class="w-[434px] h-[153px] text-[64px] font-bold text-center text-black">PASIEN ANDA</span><br /><span
-            class="w-[434px] h-[153px] text-[64px] font-bold text-center text-black">HARI INI</span>
-    </p>
-    <div class="w-[1440px] h-[95px] absolute left-0 top-[1130px]">
-        <img src="{{ asset('assets/2-4.png') }}"
-            class="w-[311px] h-[95px] absolute left-[-1px] top-[-1px] object-none" /><img
-            src="{{ asset('assets/2-4.png') }}"
-            class="w-[311px] h-[95px] absolute left-[310px] top-[-1px] object-none" /><img
-            src="{{ asset('assets/2-4.png') }}"
-            class="w-[311px] h-[95px] absolute left-[621px] top-[-1px] object-none" /><img
-            src="{{ asset('assets/2-4.png') }}"
-            class="w-[311px] h-[95px] absolute left-[932px] top-[-1px] object-none" /><img
-            src="{{ asset('assets/2-4.png') }}"
-            class="w-[311px] h-[95px] absolute left-[1243px] top-[-1px] object-none" />
-    </div>
-    <div class="w-[1292px] h-[602px]">
-        <div class="w-[1292px] h-[602px]">
-            <div class="w-[1292px] h-[602px] absolute left-[69.75px] top-[348.75px] rounded-[15px] bg-[#183e9f] border-[0.5px] border-black"
-                style="box-shadow: 12px 14px 17.899999618530273px 2px rgba(0,0,0,0.25);"></div>
-            <p class="absolute left-[110px] top-[382px] text-4xl font-semibold text-left text-white">
-                Nama
-            </p>
-            <p class="absolute left-[110px] top-[474px] text-3xl font-semibold text-left text-white">
-                Michael Anderson
-            </p>
-            <p class="w-[581px] h-[58px] absolute left-[435px] top-[474px] text-2xl font-semibold text-left text-white">
-                Demam tinggi selama tiga hari, disertai sakit kepala dan nyeri otot
-            </p>
-            <p
-                class="w-[203px] h-[34px] absolute left-[1043px] top-[474px] text-3xl font-semibold text-left text-white">
-                Antrian ke-10
-            </p>
-            <p class="absolute left-[110px] top-[566px] text-3xl font-semibold text-left text-white">
-                Sarah Williams
-            </p>
-            <p class="w-[581px] h-[58px] absolute left-[435px] top-[566px] text-2xl font-semibold text-left text-white">
-                Batuk kering lebih dari dua minggu, terkadang sulit bernapas.
-            </p>
-            <p
-                class="w-[203px] h-[34px] absolute left-[1043px] top-[566px] text-3xl font-semibold text-left text-white">
-                Antrian ke-11
-            </p>
-            <p class="absolute left-[110px] top-[658px] text-3xl font-semibold text-left text-white">
-                James Taylor
-            </p>
-            <p class="w-[581px] h-[58px] absolute left-[435px] top-[658px] text-2xl font-semibold text-left text-white">
-                Nyeri di punggung bawah, terutama saat berdiri terlalu lama.
-            </p>
-            <p
-                class="w-[203px] h-[34px] absolute left-[1043px] top-[658px] text-3xl font-semibold text-left text-white">
-                Antrian ke-12
-            </p>
-            <p class="absolute left-[110px] top-[750px] text-3xl font-semibold text-left text-white">
-                Emily Carter
-            </p>
-            <p class="w-[581px] h-[58px] absolute left-[435px] top-[750px] text-2xl font-semibold text-left text-white">
-                Ruam merah di kulit tangan yang terasa gatal, muncul sejak dua hari lalu.
-            </p>
-            <p
-                class="w-[203px] h-[34px] absolute left-[1043px] top-[750px] text-3xl font-semibold text-left text-white">
-                Antrian ke-13
-            </p>
-            <p class="absolute left-[110px] top-[842px] text-3xl font-semibold text-left text-white">
-                Daniel Foster
-            </p>
-            <p class="w-[581px] h-[58px] absolute left-[435px] top-[842px] text-2xl font-semibold text-left text-white">
-                Rasa mual setelah makan dan perut kembung selama seminggu.
-            </p>
-            <p
-                class="w-[203px] h-[34px] absolute left-[1043px] top-[842px] text-3xl font-semibold text-left text-white">
-                Antrian ke-14
-            </p>
-            <p class="absolute left-[435px] top-[382px] text-4xl font-semibold text-left text-white">
-                Keluhan Pasien
-            </p>
-            <p class="absolute left-[1046px] top-[382px] text-4xl font-semibold text-left text-white">
-                Antrian Pasien
-            </p>
-        </div>
-        <svg width="1160" height="2" viewBox="0 0 1160 2" fill="none" xmlns="http://www.w3.org/2000/svg"
-            class="absolute left-[122.49px] top-[444.01px]" preserveAspectRatio="none">
-            <line x1="-0.00543213" y1="1.00635" x2="1160.01" y2="1.00635" stroke="white"></line>
-        </svg>
-    </div>
+<h1 class="text-6xl font-bold text-center font-serif">PASIEN ANDA <br> HARI INI</h1>
 
+<!-- Blur Background -->
+<div>
+    <div class="absolute rounded-full bg-[#183e9f] w-[700px] h-[700px] blur-2xl -mt-[400px] -ml-[400px]">
+    </div>
 </div>
+
+<div class="flex justify-center mt-12 mb-12">
+    <table class=" text-sm text-left rtl:text-right rounded-lg shadow-2xl">
+        <thead class="text-xs text-white bg-[#183e9f]">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    <h1 class="text-4xl">Nama</h1>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <h1 class="text-4xl">Keluhan Pasien</h1>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <h1 class="text-4xl">Antrian Pasien</h1>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-blue-500">
+                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    <h1 class="text-3xl text-white">Michael Anderson</h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white"> Demam tinggi selama tiga hari, disertai sakit kepala dan nyeri otot
+                    </h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white">Antrian ke-10</h1>
+                </td>
+            </tr>
+            <tr class="bg-blue-500">
+                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    <h1 class="text-3xl text-white">Sarah Williams</h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white">Batuk kering lebih dari dua minggu, terkadang sulit bernapas.</h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white">Antrian ke-11</h1>
+                </td>
+            </tr>
+            <tr class="bg-blue-500">
+                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    <h1 class="text-3xl text-white">James Taylor</h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white">Nyeri di punggung bawah, terutama saat berdiri terlalu lama.</h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white">Antrian ke-12</h1>
+                </td>
+            </tr>
+            <tr class="bg-blue-500">
+                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    <h1 class="text-3xl text-white">Emily Carter</h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white">Ruam merah di kulit tangan yang terasa gatal, muncul sejak dua hari
+                        lalu.</h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white">Antrian ke-13</h1>
+                </td>
+            </tr>
+            <tr class="bg-blue-500">
+                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    <h1 class="text-3xl text-white">Daniel Foster</h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white">Rasa mual setelah makan dan perut kembung selama seminggu.</h1>
+                </td>
+                <td class="px-6 py-4">
+                    <h1 class="text-3xl text-white">Antrian ke-14</h1>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<!-- Blur Background -->
+<div class="relative overflow-x-clip">
+    <div class="relative rounded-full bg-[#183e9f] w-[700px] h-[700px] blur-2xl -mt-[400px] ml-[1600px] -z-[100]">
+    </div>
+</div>
+
+<div class="flex justify-around mt-[300px] mb-24">
+    <img src="{{ asset('assets/2-4.png') }}" alt="">
+    <img src="{{ asset('assets/2-4.png') }}" alt="">
+    <img src="{{ asset('assets/2-4.png') }}" alt="">
+    <img src="{{ asset('assets/2-4.png') }}" alt="">
+</div>
+
+
+
 @endsection
